3cea847554c33c736a9677be8115736defb54394
Dima <bugaydima@yandex.ru>
2016-09-05 23:25:59 +0300

    Вывод товаров категории

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -11,4 +11,10 @@ class CategoryController extends AppController
         $hits = Product::find()->where(['hit' => '1'])->limit(6)->asarray()->all();
         return $this->render('index', compact('hits'));
     }
+    public function actionView($id) 
+    {
+        $id = Yii::$app->request->get('id');
+        $products = Product::find()->where(['category_id' => $id])->asarray()->all();
+        return $this->render('view', compact('products'));
+    }
 }
